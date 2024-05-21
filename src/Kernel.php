14d986d94c02969aa5ad2074521de1ff581a70e1
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2024-05-22 01:08:08 +0300

    Fix paths :)

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,10 +7,10 @@ use Illuminate\Support\ServiceProvider;
 class Kernel extends ServiceProvider
 {
     public function register(): void {
-        $this->loadViewsFrom(__DIR__ .'/views', 'editor.js');
+        $this->loadViewsFrom(__DIR__ . '/views', 'editor.js');
 
         $this->publishes([
-            __DIR__ .'/views' => resource_path('views/vendor/editor.js'),
+            __DIR__ . '/views' => resource_path('views/vendor/editor.js'),
         ], 'editor.js');
     }
 }
