30680dd2dd9ed4700df3c1c0556a44a68b4f78b5
Makhlenko Sergey <mahlenko-weblive@yandex.ru>
2024-05-22 11:32:43 +0300

    Add blade directive "@editorJs"

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace Mahlenko\EditorJS;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class Kernel extends ServiceProvider
@@ -12,5 +13,11 @@ class Kernel extends ServiceProvider
         $this->publishes([
             __DIR__ . '/views' => resource_path('views/vendor/editor.js'),
         ], 'editor.js');
+    }
+
+    public function boot(): void{
+        Blade::directive('editorJs', function($expression) {
+            return "<?php echo \Mahlenko\EditorJS\Render::make($expression); ?>";
+        });
     }
 }
--- src/Render.php
+++ src/Render.php
@@ -5,8 +5,18 @@ namespace Mahlenko\EditorJS;
 use Illuminate\Support\Facades\View;
 
 class Render {
-    static public function make(string $json, string $prefix = 'editor__') {
-        return View::make('editor.js::content', (array) json_decode($json))
+    static public function make($json, string $prefix = 'editor__') {
+        if (is_string($json)) {
+            $json = json_decode($json);
+        } else {
+            $json = json_decode(json_encode($json));
+        }
+
+        if (!isset($json->blocks)) {
+            return '<p style="font-size: 0.86rem; font-weight: bold; margin: .5rem 0;">⚠️ The content does not match Editor.JS</p>';
+        }
+
+        return View::make('editor.js::content', (array) $json)
             ->with('prefix', $prefix);
     }
 }
